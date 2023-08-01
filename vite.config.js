d4fb686b3b0f438b8c10fd63c5fff554548ac06f
24min <hdu424fan@qq.com>
2023-08-01 20:22:07 +0800

    🍚 打包文件名的问题

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 const path = require("path");
 const buildDir = 'build';
-
+const outputFileName = {
+    bg: 'store',
+    content: 'refreshConfigPage',
+    popup: 'popup'
+}
 module.exports = {
-    build:{
-        outDir:`./${buildDir}`,
-        rollupOptions:{
-            input:{
-                store:path.resolve(__dirname,'./bg/store.js'),
-                content:path.resolve(__dirname,'./content/refreshConfigPage.js')
+    build: {
+        outDir: `./${buildDir}`,
+        rollupOptions: {
+            input: {
+                bg: path.resolve(__dirname, './bg/store.js'),
+                content: path.resolve(__dirname, './content/refreshConfigPage.js'),
+                popup: path.resolve(__dirname, './popup/popup.js')
             },
-            output:{
-                entryFileNames: "[name]/[name].js",// 打包的文件名
-                assetFileNames:"[name]",
-                chunkFileNames:"[name].js"
+            output: {
+                entryFileNames: (chunkInfo) => {
+                    return `${chunkInfo.name}/${outputFileName[chunkInfo.name]}.js`
+                },// 打包的文件名
             }
-
         }
-        
     }
 }
